b42c61a815eb7e06404e5c2d47a164924d739753
liuxingmoon <810136630@qq.com>
2020-09-17 23:06:45 +0800

    2020

---
+++ weekcheck.py
@@ -0,0 +1,25 @@
+#生成周报
+import pandas as pd
+import configparser
+import os
+
+configpath = r"D:\Program Files\Python38\works\tools\Config.ini"
+
+config = configparser.ConfigParser()
+config.read(configpath, encoding="utf-8")
+
+dailycheckPath = config.get("work", "dailycheckPath")
+weekcheckPath = config.get("work", "weekcheckPath")
+
+#打开日报，复制空行以上的信息
+with open(dailycheckPath,mode='r') as dailycheckfile:
+    lines = dailycheckfile.readlines()
+    lineendindex = lines.index('\n')
+    line=[]
+    for n in range(0,lineendindex):
+        line.append(lines[n])
+    print(line)
+
+os.chdir(weekcheckPath)
+df = pd.read_excel(r'工作周报-刘兴20200917.xlsx',sheet_name=0)
+print(df)
