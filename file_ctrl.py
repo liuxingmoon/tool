b124cb81200df23ee53ba05e6063c4255c34a6cd
scrcu <810136630@qq.com>
2022-01-17 16:06:02 +0800

    20220117

--- file_ctrl.py
+++ file_ctrl.py
@@ -1,6 +1,80 @@
 #用于处理文件
 import os
+import win32com
+from win32com.client import Dispatch
 
+# 处理Word文档的类
+class RemoteWord:
+    def __init__(self, filename=None):
+        self.xlApp = win32com.client.Dispatch('Word.Application') # 此处使用的是Dispatch，原文中使用的DispatchEx会报错
+        self.xlApp.Visible = 0 # 后台运行，不显示
+        self.xlApp.DisplayAlerts = 0  #不警告
+        if filename:
+            self.filename = filename
+            if os.path.exists(self.filename):
+                self.doc = self.xlApp.Documents.Open(filename)
+            else:
+                self.doc = self.xlApp.Documents.Add()  # 创建新的文档
+                self.doc.SaveAs(filename)
+        else:
+            self.doc = self.xlApp.Documents.Add()
+            self.filename = ''
+  
+    def add_doc_end(self, string):
+        '''在文档末尾添加内容'''
+        rangee = self.doc.Range()
+        rangee.InsertAfter('\n' + string)
+  
+    def add_doc_start(self, string):
+        '''在文档开头添加内容'''
+        rangee = self.doc.Range(0, 0)
+        rangee.InsertBefore(string + '\n')
+  
+    def insert_doc(self, insertPos, string):
+        '''在文档insertPos位置添加内容'''
+        rangee = self.doc.Range(0, insertPos)
+        if (insertPos == 0):
+            rangee.InsertAfter(string)
+        else:
+            rangee.InsertAfter('\n' + string)
+  
+    def replace_doc(self, string, new_string):
+        '''替换文字'''
+        self.xlApp.Selection.Find.ClearFormatting()
+        self.xlApp.Selection.Find.Replacement.ClearFormatting()
+        #(string--搜索文本,
+        # True--区分大小写,
+        # True--完全匹配的单词，并非单词中的部分（全字匹配）,
+        # True--使用通配符,
+        # True--同音,
+        # True--查找单词的各种形式,
+        # True--向文档尾部搜索,
+        # 1,
+        # True--带格式的文本,
+        # new_string--替换文本,
+        # 2--替换个数（全部替换）
+        self.xlApp.Selection.Find.Execute(string, False, False, False, False, False, True, 1, True, new_string, 2)
+  
+    def replace_docs(self, string, new_string):
+        '''采用通配符匹配替换'''
+        self.xlApp.Selection.Find.ClearFormatting()
+        self.xlApp.Selection.Find.Replacement.ClearFormatting()
+        self.xlApp.Selection.Find.Execute(string, False, False, True, False, False, False, 1, False, new_string, 2)
+    def save(self):
+        '''保存文档'''
+        self.doc.Save()
+  
+    def save_as(self, filename):
+        '''文档另存为'''
+        self.doc.SaveAs(filename)
+  
+    def close(self):
+        '''保存文件、关闭文件'''
+        self.save()
+        self.xlApp.Documents.Close()
+        self.xlApp.Quit()
+  
+  
 #重命名文件
 def rename(filename_last,filename_new):
     try:
--- statistics_dev.py
+++ statistics_dev.py
@@ -1,6 +1,7 @@
 #农信统计故障设备
 from xlsx_ctrl import *
 from config_ctrl import config_read
+from file_ctrl import copy_file,RemoteWord
 import easygui as g
 import time,re,os,datetime
 import subprocess
@@ -8,9 +9,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import win32gui
 from win32.lib import win32con
-from read_config import configpath
 
-excelFile = config_read(configpath,'work','硬件故障记录')
+configpath = "Config.ini"
+workdir = config_read(configpath,'work','workdir')
+excelFile = "硬件故障记录.xlsx"
+pangudisk_template = "pangu磁盘白屏更换流程.docx"
 commit_person = config_read(configpath,'work','commit_person')
 cmdb_url = r"http://10.128.128.238/report/cmdb/v_server"
 wps_url_dev_report = r"http://10.0.28.95/view/p/57001"
@@ -24,13 +27,8 @@ environment_xpath = r"/html/body/div[2]/div[2]/div/div[3]/div[2]/table/tbody/tr/
 fix_start_time_xpath = r"/html/body/div[2]/div[2]/div/div[3]/div[2]/table/tbody/tr/td[30]"
 fix_end_time_xpath = r"/html/body/div[2]/div[2]/div/div[3]/div[2]/table/tbody/tr/td[31]"
 fix_year_path = r"/html/body/div[2]/div[2]/div/div[3]/div[2]/table/tbody/tr/td[32]"
-
-excelFile = r"%s" %(excelFile)
-#excelFile = repr(excelFile)
-#excelFile = excelFile.replace("\\","\\").replace("\t","\\t")
-#excelFile = r"W:\工单\硬件故障记录.xlsx"
-
-#excelFile = r"C:\Users\Administrator.PC-20200904VSVM\Desktop\export\device_error_info.xlsx"
+workdir = r"%s" %(workdir)
+programdir = os.getcwd()
 
 def openfile(excelFile):
     subprocess.Popen(excelFile,shell=True)
@@ -62,7 +60,6 @@ def get_info_cmdb(nc_sn):
     factory_name = driver.find_element_by_xpath(factory_name_xpath).text
     devicename = driver.find_element_by_xpath(devicename_xpath).text
     room = driver.find_element_by_xpath(room_xpath).text
-    
     rack_cmdb = driver.find_element_by_xpath(rack_xpath).text.split('-')[-1]
     if rack_cmdb.isdigit():#只由数字，加上倒数第二个字符
         rack_cmdb = driver.find_element_by_xpath(rack_xpath).text.split('-')[-2] + rack_cmdb
@@ -99,6 +96,7 @@ def start():
     try:#getinfo 对接3个参数的磁盘信息
         cluster = re.findall("Local_cluster:.*",device_error_info)[0].split(": ")[1]
         hostname = re.findall("Local_hostname:.*",device_error_info)[0].split(": ")[1]
+        IP_address = re.findall("Local_address:.*",device_error_info)[0].split(": ")[1]
         nc_sn = re.findall("Serial Number:.*",device_error_info)[0].split(": ")[1]
         try:
             disk_dev = re.findall(".*disk.*/sd.*",device_error_info)[0].strip("\/")
@@ -167,6 +165,27 @@ def start():
     #保存报告信息
     report_itsm_info = r'%s环境更换云平台%s集群%s %s的机器%s的%s %s' %(environment,cluster,room,rack_unit,devicename,error_dev,error_dev_info)
     report_daily_info = r'%s服务器%s,于%s经%s，涉及%s云%s集群，于 %s 更换故障硬盘后恢复正常，未影响系统可用性。' %(factory_name,devicename,time_report,remarks,environment,cluster,time_fix)
+    if error_dev == "硬盘":
+        today_time = datetime.datetime.today().strftime("%Y%m%d")
+        filename = "%s-%s %s硬盘故障"%(today_time,hostname,disk_dev)
+        filename = filename.replace("/"," ")
+        global workdir,programdir
+        workdir = workdir + os.sep + filename
+        copy_file(pangudisk_template,programdir,workdir)
+        filename_new = workdir + os.sep + pangudisk_template
+        pos = room + " " + rack_unit
+        doc = RemoteWord(filename_new)  # 初始化一个doc对象
+        doc.replace_doc("生产", environment)
+        doc.replace_doc("ECS-IO8River-B-5de2", cluster)
+        doc.replace_doc("nxba09506.cloud.a14.qm500", hostname)
+        doc.replace_doc("14.0.0.66", IP_address)
+        doc.replace_doc("西信-西区 1F-NXBW-A-14",pos)
+        doc.replace_doc("819603773",nc_sn)
+        doc.replace_doc("Sde/disk3",devicename)
+        doc.replace_doc("SA5212A074",disk_dev)
+        doc.replace_doc("Device Model:     ST8000NM0055-1RM112 + '\n' + Serial Number:    ZA1HBDEB",disk_info)
+        doc.close()
     with open('report_dev.txt','w') as rp:
         rp.write(report_itsm_info + '\n' + report_daily_info)
     os.system('report_dev.txt')
+
